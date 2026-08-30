82336d4cf7edc275f7e80ec31fbfd48df59b0357
yash_ohol <yashohol1362006@gmail.com>
2026-08-30 23:05:47 +0530

    Update eda.py

--- ChurnGuard/src/eda.py
+++ ChurnGuard/src/eda.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
 import seaborn as sns 
-
+ 
 df= pd.read_csv(r"C:\Users\Yash Ohol\OneDrive\ChurnGuard\data\WA_Fn-UseC_-Telco-Customer-Churn.csv")
 # print(df)
 
